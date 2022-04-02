68b71e1ae4e2c045123783fac4e5e944672e8da7
Thamognya Kodi <contact@thamognya.com>
2022-04-02 22:53:16 +0700

    added scripts

---
+++ scripts/blog-compile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cd ./src/blog-src/ && zola build && git add . && git commit -m 'blog.thamognya.com: auto-update' && git remote | xargs -L1 git push --all 
---
+++ scripts/thamognya-compile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cd ./src/thamognya-src/ && npm run build && cp -r ./build/* ../thamognya/ && git add . && git commit -m 'www.thamognya.com : auto-update' && git remote | xargs -L1 git push --all 
