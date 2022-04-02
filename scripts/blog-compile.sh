598c9df06fb83d70e79a5b6222e8dd96dbb872db
Thamognya Kodi <contact@thamognya.com>
2022-04-02 23:05:23 +0700

    blog-update auto update

--- scripts/blog-compile.sh
+++ scripts/blog-compile.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cd ./src/blog-src/ && zola build && git add . && git commit -m 'blog.thamognya.com: auto-update' && git remote | xargs -L1 git push --all 
+cd ./src/blog-src/ && zola build  
+#git add . && git commit -m 'blog.thamognya.com: auto-update' && git remote | xargs -L1 git push --all 
